639a2ccdc0063957e433b7a2a00765bb8a005950
Thabani Takwena <129037720+thabanidev@users.noreply.github.com>
2024-03-14 07:52:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import pandas as pd
-from newsGetter import get_news
-from sentimentAnalysis import get_sentiment
-
 
 PG = pd.read_csv('PG.csv', parse_dates=True)
 PG['Date'] = pd.to_datetime(PG['Date'], format='%Y-%m-%d')
@@ -11,8 +8,3 @@ print(PG.head(100))
 # Drop the rows where the column 'opening' is equal to 0.0
 PG = PG[PG["Open"] != 0.0]
 print(PG.head(100))
-
-PG['news'] = PG['Date'].apply(get_news)
-PG["sentiment"] = PG["news"].apply(get_sentiment)
-
-print(PG.head(50))
